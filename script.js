5b735ab70341bcfe467bce36ce0e51f285a1bb0c
sakshinig <117240256+sakshinig@users.noreply.github.com>
2023-05-27 04:34:23 +0000

    Replaced Video and made some bug fixes

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ var callAPI = ()=>{
   .then(response => response.text())
   .then(result => {
     var str = JSON.parse(result).body;
-    var isViolent = str.includes("Outer Space");
-    var isVisuallyDistrbing = str.includes("Visually Distrubing");
+    var isViolent = str.includes("Riot");
+    var isFire = str.includes("Gun");
     var ViolenceMessage = document.getElementById('violence');
     var ViD = document.getElementById('vd');
     var Nothing = document.getElementById('nothing');
@@ -32,11 +32,14 @@ var callAPI = ()=>{
       Nothing.style.display = 'none';
       ViolenceMessage.style.display = 'block';
       Loading.style.display = 'none';
+      document.getElementById("rcscore").innerHTML = str.slice(str.indexOf("Riot")+4+14, str.indexOf("Riot")+4+19) + "%";
     }
-    if (isVisuallyDisturbing){
+    if (isFire){
       Nothing.style.display = 'none';
       ViD.style.display = 'block';
       Loading.style.display = 'none';
+      document.getElementById("cscore").innerHTML = str.slice(str.indexOf("Gun")+3+14, str.indexOf("Gun")+3+14+5) + "%";
+      
     }
     
   })
